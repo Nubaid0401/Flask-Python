e595d6e7934b912e50a1f84171d4a0eacc69afae
Nubaid Amin <nubaid.amin@gmail.com>
2026-09-16 22:59:06 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/calculate", methods=["POST"])
-def calculate():
-    units = int(request.form["units"])
-    bill = units * 5
-
-    if units <= 100:
-        message = "Great! You are an energy saver!"
-    elif units <= 200:
-        message = "Not bad! Try saving a little more!"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        mydb = mysql.connector.connect(
+            host = 'remotemyql.com',
+            user = 'Rz8hqnldk4',
+            password = 'nd0wK03xe0',
+            database = 'Rz8hqnldk4'
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute('SELECT * FROM LoginDetails WHERE Name = %s AND Password = %s', (username, password))
+        account = mycursor.fetchone()
+        if account:
+            print("login success")
+            name = account[1]
+            id = account[0]
+            msg = 'Logged in Successfully'
+            print('login successful')
+            return render_template('index.html', msg=msg, name=name, id=id)
+        else:
+            msg = 'Incorrect Credentials. Kindly check.'
+            return render_template('login.html', msg=msg)
     else:
-        message = "Whoa! Time to switch off some lights!"
+        return render_template('login.html')
 
-    return render_template("index.html", units=units, bill=bill, message=message)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/logout')
+def logout():
+    name = ''
+    id = ''
+    msg = 'Logged out succesfully'
+    return render_template('login.html',msg=msg, name=name, id=id)
